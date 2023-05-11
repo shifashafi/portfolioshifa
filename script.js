183693bf310ceb150e2e62020c581a6c40f877d2
shifashafi <84844061+shifashafi@users.noreply.github.com>
2023-05-11 15:23:48 +0530

    added goto top func

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 let sections = document.querySelectorAll("skills");
 let navLinks = document.querySelectorAll("social-icons nav a");
+let topButton = document.querySelector('top-btn');
+
+function scrollToTop() {
+    window.scrollTo(0, 0);
+  }
