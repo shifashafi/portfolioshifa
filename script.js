16048c932d9802c8cb98526fead36a944274331b
shifashafi <84844061+shifashafi@users.noreply.github.com>
2023-05-11 15:27:37 +0530

    updated onclick func

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let sections = document.querySelectorAll("skills");
 let navLinks = document.querySelectorAll("social-icons nav a");
 let topButton = document.querySelector('top-btn');
 
-function scrollToTop() {
-    window.scrollTo(0, 0);
-  }
+topButton.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+});
